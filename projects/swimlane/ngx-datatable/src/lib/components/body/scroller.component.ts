608ad9c31d7c6b98616d00932d150f28bf1423af
Joe-EdgeTech <joe.firebaugh@edge-technologies.com>
2020-04-28 14:54:42 -0400

    Fix scrolling issue that I injected by removing the scroll listener from ngzone

--- projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/body/scroller.component.ts
@@ -58,10 +58,7 @@ export class ScrollerComponent implements OnInit, OnDestroy {
       const renderer = this.renderer;
       this.parentElement = renderer.parentNode(renderer.parentNode(this.element));
       this._scrollEventListener = this.onScrolled.bind(this);
-      this.ngZone.runOutsideAngular(() => {
-        this.parentElement.addEventListener('scroll', this._scrollEventListener);
-        }
-      )
+      this.parentElement.addEventListener('scroll', this._scrollEventListener);
     }
   }
 
